a72ad27b66487f82c027ff25eabc23b1f8887389
Birkir Gudjonsson <birkir.gudjonsson@gmail.com>
2019-01-11 14:58:10 -0500

    v1.0.3

---
+++ overwrites/cra/index.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Helmet from 'react-helmet';
+
+import { AppLayout } from './components/app-layout/AppLayout';
+import { Home } from './routes/Home';
+import { About } from './routes/About';
+import { NotFound } from './routes/404';
+import * as serviceWorker from './serviceWorker';
+
+const App = () => (
+  <Router>
+    <Route
+      render={() => (
+        <AppLayout>
+          <Helmet>
+            <html lang="en" />
+            <title>Hello</title>
+          </Helmet>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/about" component={About} />
+            <Route component={NotFound} />
+          </Switch>
+        </AppLayout>
+      )}
+    />
+  </Router>
+)
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
