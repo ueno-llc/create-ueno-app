c142e50e3f89eec7bbc4f94505627e3754cce1e4
Jérémy Barbet <jeremgraph@gmail.com>
2019-01-23 15:38:11 +0000

    Update gatsby overwrite link

--- overwrites/gatsby/Link.tsx
+++ overwrites/gatsby/Link.tsx
@@ -1,12 +1,8 @@
 import * as React from 'react';
 import { Link as GatsbyLink } from 'gatsby';
 
-export const Link = ({ children, ...props }: any) => {
-  const child = React.Children.only(children);
-
-  return (
-    <GatsbyLink {...props}>
-      {child.props.children}
-    </GatsbyLink>
-  );
-};
+export const Link = ({ children, ...props }: any) => (
+  <GatsbyLink {...props}>
+    {children}
+  </GatsbyLink>
+);
