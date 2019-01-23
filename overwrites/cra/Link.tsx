9b67d07617830e33d4d21d30b9f953bb608c7267
Jérémy Barbet <jeremgraph@gmail.com>
2019-01-23 15:53:04 +0000

    Change cra overwrite

--- overwrites/cra/Link.tsx
+++ overwrites/cra/Link.tsx
@@ -1,12 +1,8 @@
 import * as React from 'react';
 import { Link as RouterLink } from 'react-router-dom';
 
-export const Link = ({ children, ...props }: any) => {
-  const child = React.Children.only(children);
-
-  return (
-    <RouterLink {...props}>
-      {child.props.children}
-    </RouterLink>
-  );
-};
+export const Link = ({ children, ...props }: any) => (
+  <RouterLink {...props}>
+    {children}
+  </RouterLink>
+);
